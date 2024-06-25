4f4bf6e2d2cb5fe4f14a3f410442795fa6651e43
Kristina.Kuznetsova <kzkristina90@gmail.com>
2024-06-25 12:45:32 +0300

    initial

---
+++ src/main/java/com/example/testwsl/TestWslApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testwsl/TestWslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testwsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
